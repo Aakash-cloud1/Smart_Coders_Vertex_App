9d2341220a09a5236f8acc82fc5237be8cfcccb8
Attuluri Aakash <Attuluri.Aakash@iiitb.ac.in>
2025-12-10 15:28:44 +0530

    Delete quiz_app_full directory

--- quiz_app_full/src/main/java/com/example/quizapp/model/Admin.java
+++
@@ -1,8 +0,0 @@
-package com.example.quizapp.model;
-
-public class Admin extends User {
-    public Admin() {}
-    public Admin(int id, String username, String passwordHash, String role) {
-        super(id, username, passwordHash, role);
-    }
-}
--- quiz_app_full/src/main/java/com/example/quizapp/model/Student.java
+++
@@ -1,8 +0,0 @@
-package com.example.quizapp.model;
-
-public class Student extends User {
-    public Student() {}
-    public Student(int id, String username, String passwordHash, String role) {
-        super(id, username, passwordHash, role);
-    }
-}
